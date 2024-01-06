a874b5564bb9e1801c05ea94a676f8d95aebf1a8
syed-ayman-quadri <87442765+syed-ayman-quadri@users.noreply.github.com>
2024-01-06 15:44:47 -0700

    Completed Login and Signup Functions

--- hackathon_manager/hackathon_manager/urls.py
+++ hackathon_manager/hackathon_manager/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teams/', include('teams.urls')),
+    path('teams/', include('teams.urls', namespace='teams')),
 ]
